e8379b9a13fc64c2518184fa97117fa8761a3a97
ognick <ogneslav.work@gmail.com>
2017-05-09 17:46:17 +0300

    fix duplicate

--- answerer.py
+++ answerer.py
@@ -7,6 +7,7 @@ import sys
 from logging import getLogger
 import requests
 import json
+import random
 import vk
 from settings import settings
 from sys_utilst import load, save
@@ -23,9 +24,9 @@ logger = getLogger(__name__)
 MAX_USERS_PER_REQUEST = 1000
 MAX_MESSAGES_PER_REQUEST = 200
 
-NOT_FOLLOW_MSG = u'Сначала вступи в этот паблик.'
-BAD_WORDS_MSG= u'Я таких слов не знаю.'
-NO_BLOCKS_MSG=u'Что-то об этом мало пишут.'
+NOT_FOLLOW_MSG = u'Сначала вступи в паблик.'
+BAD_WORDS_MSG = u'Я таких слов не знаю.'
+NO_BLOCKS_MSG = u'Об этом мало пишут.'
 
 
 def get_all_items(max_count_per_request, getter):
@@ -40,6 +41,24 @@ def get_all_items(max_count_per_request, getter):
         time.sleep(settings['sleep'])
 
     return items
+
+
+def send_image(api, user_id, sign, post, title):
+    # if user_id not in settings['tester_ids']:
+    #     return
+
+    generate_image(post, sign=sign, title=title, result_file='img.jpg')
+    img = {'photo': ('img.png', open(r'img.jpg', 'rb'))}
+    upload_url = api.photos.getMessagesUploadServer()['upload_url']
+    response = json.loads(requests.post(upload_url, files=img).text)
+
+    photo_data = api.photos.saveMessagesPhoto(photo=response['photo'],
+                                              hash=response['hash'],
+                                              server=response['server'])
+
+    attach = 'photo%s_%s' % (photo_data[0]['owner_id'], photo_data[0]['id'])
+    api.messages.send(user_id=user_id, attachment=[attach])
+    time.sleep(settings['sleep'])
 
 
 def answer(source_file_name):
@@ -66,24 +85,6 @@ def answer(source_file_name):
         MAX_MESSAGES_PER_REQUEST,
         lambda **kwargs : api.messages.getDialogs(unanswered=1, preview_length=20, **kwargs)
     )
-
-    def _answer(uid, sign, post, title):
-        logger.info('sending to %s %s' % (sign, title))
-        # if uid not in settings['tester_ids']:
-        #     return
-
-        generate_image(post, sign=sign, title=title, result_file='img.jpg')
-        img = {'photo': ('img.png', open(r'img.jpg', 'rb'))}
-        upload_url = api.photos.getMessagesUploadServer()['upload_url']
-        response = json.loads(requests.post(upload_url, files=img).text)
-
-        photo_data = api.photos.saveMessagesPhoto(photo=response['photo'],
-                                                  hash=response['hash'],
-                                                  server=response['server'])
-
-        attach = 'photo%s_%s' % (photo_data[0]['owner_id'], photo_data[0]['id'])
-        api.messages.send(user_id=uid, attachment=[attach])
-        time.sleep(settings['sleep'])
 
     for item in messages:
         msg = item['message']
@@ -116,7 +117,8 @@ def answer(source_file_name):
 
         curr_user_cache = used_cache.setdefault(user_id, set())
         while blocks:
-            block = blocks.pop()
+            block_id = random.randint(0, len(blocks)) if user_id in settings['tester_ids'] else 0
+            block = blocks.pop(block_id)
             post = []
             is_author = {user_id: True}
 
@@ -139,12 +141,13 @@ def answer(source_file_name):
 
                 try:
                     sign = followers[user_id] if is_cached else init_sign
-                    _answer(user_id, sign, post, title)
+                    send_image(api, user_id, sign, post, title)
                     if is_cached:
                         user_cache |= s_post
                         save(used_cache, 'used_cache.bin')
+                    logger.info('send to %s' % sign)
                 except vk.exceptions.VkAPIError as error:
-                    logger.error('%s %s' %(sign, error.message))
+                    logger.error('send to %s %s' %(sign, error.message))
                     continue
             break
         else:
