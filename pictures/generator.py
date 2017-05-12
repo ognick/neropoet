47d5435cc4ac1f7706ee63d2c2591047af2ee37f
ognick <ogneslav.work@gmail.com>
2017-05-12 17:44:27 +0300

    fix template

--- pictures/generator.py
+++ pictures/generator.py
@@ -19,22 +19,25 @@ MAX_TEXT_WIDTH = 2000
 
 
 TEMPLATES = {
-    720 : {
+    700 : {
         'image': Image.open('pictures/small.jpg').copy(),
         'start_pos_y': 140,
-        'min_offset_x': 40,
+        'sign_offset_x': 40,
+        'left_offset_x': 0,
         'align': ALIGN.CENTER,
     },
-    880 : {
+    854 : {
         'image': Image.open('pictures/middle.jpg').copy(),
         'start_pos_y': 140,
-        'min_offset_x': 40,
+        'sign_offset_x': 40,
+        'left_offset_x': 50,
         'align': ALIGN.CENTER,
     },
     MAX_TEXT_WIDTH: {
         'image': Image.open('pictures/big.jpg').copy(),
         'start_pos_y': 140,
-        'min_offset_x': 40,
+        'sign_offset_x': 20,
+        'left_offset_x': 80,
         'align': ALIGN.CENTER,
     },
 }
@@ -64,34 +67,38 @@ def generate_image(lines, sign=None, title=None, show=False):
     else:
         template = TEMPLATES[MAX_TEXT_WIDTH]
 
-    min_offset_x = template['min_offset_x']
+    sign_offset_x = template['sign_offset_x']
+    left_offset_x = template['left_offset_x']
     img = template['image'].copy()
     draw = ImageDraw.Draw(img)
     max_text_width = img.size[0]
     align = template['align']
-    fixed_x = None if align == ALIGN.CENTER else (max_text_width - max_width) / 2
+    fixed_x = None if align == ALIGN.CENTER else (max_text_width - max_width) / 2 - 40
 
     if title:
         w = _draw.textsize(title, font=_common_font)[0]
-        x = (max_text_width - w) / 2
+        x = (max_text_width - w) / 2 - left_offset_x
         draw.text((x, 60), up_and_dot(title), COMMON_COLOR, font=_common_font)
 
 
     for i, line in enumerate(lines):
         y = template['start_pos_y'] + i * LINE_DISTANCE
+        x = 0
         if align == ALIGN.LEFT:
             x = fixed_x
         elif align == ALIGN.CENTER:
             x = (max_text_width - widths[i]) / 2
         elif align == ALIGN.RIGHT:
             x = (max_width - widths[i]) + fixed_x
+
+        x -= left_offset_x
         draw.text((x, y), line, COMMON_COLOR, font=_common_font)
 
     y = img.size[1] - 80
-    draw.text((min_offset_x, y), 'https://vk.com/%s' % settings['group_id'],  COMMON_COLOR, font=_sign_font)
+    draw.text((sign_offset_x, y), 'https://vk.com/%s' % settings['group_id'],  COMMON_COLOR, font=_sign_font)
     if sign:
         y -= 40
-        draw.text((min_offset_x, y), sign + u' ©', COMMON_COLOR, font=_sign_font)
+        draw.text((sign_offset_x, y), sign + u' ©', COMMON_COLOR, font=_sign_font)
 
     if not show:
         b_arr = io.BytesIO()
@@ -99,3 +106,8 @@ def generate_image(lines, sign=None, title=None, show=False):
         return b_arr.getvalue()
     else:
         img.show()
+
+if __name__ == '__main__':
+    lines = [u'Ё' * 30]*3
+    lines.append(u'я' * 32)
+    generate_image(lines, sign='Ogneslav Arlovsky', title='Title', show=True)
