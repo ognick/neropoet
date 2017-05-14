d21b6cdb987f49aacaac2f3646db5fa085157409
ognick <ogneslav.work@gmail.com>
2017-05-14 22:04:40 +0300

    fix req

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-from credentials import credentials
 settings = {
+    'group_id': 'neropoet',
     'max_post_count': 100,
     'max_block_count': 10,
     'sleep': 0.30,
@@ -9,5 +9,28 @@ settings = {
     'processes': 4,
     'answer': 10,
     'reload': 1200,
+
+    'blacklist': [349746767],
+    'tester_ids': [4294291],
 }
-settings.update(credentials)
+
+try:
+    from credentials import credentials
+    settings.update(credentials)
+except ImportError:
+    pass
+
+try:
+    import os
+    env = os.environ
+    settings.update({
+        'group_token': env.get('group_token'),
+        'bot_auth' : {
+            'app_id': env.get('app_id'),
+            'user_login': env.get('user_login'),
+            'user_password': env.get('user_password'),
+        },
+    })
+except:
+    pass
+
